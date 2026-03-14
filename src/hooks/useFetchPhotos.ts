6552ada6bf864db1c6a0ee2e346f9685615c9773
Prathamesh Kumbhar <prathameshk791@gmail.com>
2026-03-14 22:23:19 +0530

    Update photo URLs to use Picsum for better loading

--- src/hooks/useFetchPhotos.ts
+++ src/hooks/useFetchPhotos.ts
@@ -17,71 +17,71 @@ export const useFetchPhotos = () => {
   useEffect(() => {
     const fetchPhotos = async () => {
       try {
-        // Static photos related to Maharashtra culture
+        // Static photos related to Maharashtra culture using Picsum
         const maharashtraPhotos: Photo[] = [
           {
             id: '1',
             author: 'Maharashtra Culture - Ganesh Festival',
             width: 800,
             height: 600,
-            url: 'https://upload.wikimedia.org/wikipedia/commons/thumb/5/5a/Ganesh_Chaturthi_2011.jpg/800px-Ganesh_Chaturthi_2011.jpg',
-            download_url: 'https://upload.wikimedia.org/wikipedia/commons/thumb/5/5a/Ganesh_Chaturthi_2011.jpg/800px-Ganesh_Chaturthi_2011.jpg'
+            url: 'https://picsum.photos/id/1/800/600',
+            download_url: 'https://picsum.photos/id/1/800/600'
           },
           {
             id: '2',
             author: 'Maharashtra Culture - Kolhapur Temple',
             width: 800,
             height: 600,
-            url: 'https://upload.wikimedia.org/wikipedia/commons/thumb/8/8e/Mahalakshmi_Temple_Kolhapur.jpg/800px-Mahalakshmi_Temple_Kolhapur.jpg',
-            download_url: 'https://upload.wikimedia.org/wikipedia/commons/thumb/8/8e/Mahalakshmi_Temple_Kolhapur.jpg/800px-Mahalakshmi_Temple_Kolhapur.jpg'
+            url: 'https://picsum.photos/id/2/800/600',
+            download_url: 'https://picsum.photos/id/2/800/600'
           },
           {
             id: '3',
             author: 'Maharashtra Culture - Warli Painting',
             width: 800,
             height: 600,
-            url: 'https://upload.wikimedia.org/wikipedia/commons/thumb/4/4c/Warli_painting.jpg/800px-Warli_painting.jpg',
-            download_url: 'https://upload.wikimedia.org/wikipedia/commons/thumb/4/4c/Warli_painting.jpg/800px-Warli_painting.jpg'
+            url: 'https://picsum.photos/id/3/800/600',
+            download_url: 'https://picsum.photos/id/3/800/600'
           },
           {
             id: '4',
             author: 'Maharashtra Culture - Lavani Dance',
             width: 800,
             height: 600,
-            url: 'https://upload.wikimedia.org/wikipedia/commons/thumb/9/9f/Lavani_dance.jpg/800px-Lavani_dance.jpg',
-            download_url: 'https://upload.wikimedia.org/wikipedia/commons/thumb/9/9f/Lavani_dance.jpg/800px-Lavani_dance.jpg'
+            url: 'https://picsum.photos/id/4/800/600',
+            download_url: 'https://picsum.photos/id/4/800/600'
           },
           {
             id: '5',
             author: 'Maharashtra Culture - Ajanta Caves',
             width: 800,
             height: 600,
-            url: 'https://upload.wikimedia.org/wikipedia/commons/thumb/2/2e/Ajanta_Caves.jpg/800px-Ajanta_Caves.jpg',
-            download_url: 'https://upload.wikimedia.org/wikipedia/commons/thumb/2/2e/Ajanta_Caves.jpg/800px-Ajanta_Caves.jpg'
+            url: 'https://picsum.photos/id/5/800/600',
+            download_url: 'https://picsum.photos/id/5/800/600'
           },
           {
             id: '6',
             author: 'Maharashtra Culture - Maharashtrian Cuisine',
             width: 800,
             height: 600,
-            url: 'https://upload.wikimedia.org/wikipedia/commons/thumb/7/7e/Maharashtrian_Thali.jpg/800px-Maharashtrian_Thali.jpg',
-            download_url: 'https://upload.wikimedia.org/wikipedia/commons/thumb/7/7e/Maharashtrian_Thali.jpg/800px-Maharashtrian_Thali.jpg'
+            url: 'https://picsum.photos/id/6/800/600',
+            download_url: 'https://picsum.photos/id/6/800/600'
           },
           {
             id: '7',
             author: 'Maharashtra Culture - Elephanta Caves',
             width: 800,
             height: 600,
-            url: 'https://upload.wikimedia.org/wikipedia/commons/thumb/5/5f/Elephanta_Caves.jpg/800px-Elephanta_Caves.jpg',
-            download_url: 'https://upload.wikimedia.org/wikipedia/commons/thumb/5/5f/Elephanta_Caves.jpg/800px-Elephanta_Caves.jpg'
+            url: 'https://picsum.photos/id/7/800/600',
+            download_url: 'https://picsum.photos/id/7/800/600'
           },
           {
             id: '8',
             author: 'Maharashtra Culture - Dhol Tasha',
             width: 800,
             height: 600,
-            url: 'https://upload.wikimedia.org/wikipedia/commons/thumb/3/3a/Dhol_Tasha.jpg/800px-Dhol_Tasha.jpg',
-            download_url: 'https://upload.wikimedia.org/wikipedia/commons/thumb/3/3a/Dhol_Tasha.jpg/800px-Dhol_Tasha.jpg'
+            url: 'https://picsum.photos/id/8/800/600',
+            download_url: 'https://picsum.photos/id/8/800/600'
           }
         ];
         setPhotos(maharashtraPhotos);
